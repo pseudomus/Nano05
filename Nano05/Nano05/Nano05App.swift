2b4da17d1668c961e3fc820db287715d56cedddb
Luca Lacerda <lucagabriel520@gmail.com>
2024-02-26 15:37:12 -0300

    projectAdded

---
+++ Nano05/Nano05/Nano05App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano05App.swift
+//  Nano05
+//
+//  Created by Luca Lacerda on 26/02/24.
+//
+
+import SwiftUI
+
+@main
+struct Nano05App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
